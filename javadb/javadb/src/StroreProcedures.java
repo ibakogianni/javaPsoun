3ecb7a7c160e74b442fa1bde5e78f7fb23f75a4b
Irida Bakogianni <ibakogianni@gmail.com>
2023-07-27 21:32:10 +0300

    End of video JDBC(MySQL) - ΜΑΘΗΜΑ 2 - ΣΥΝΔΕΣΗ-ΕΡΩΤΗΜΑΤΑ - Μέρος 2 από 5 - Κλήση Αποθηκευμένων Διαδικασιών

--- javadb/javadb/src/StroreProcedures.java
+++ javadb/javadb/src/StroreProcedures.java
@@ -1,3 +1,27 @@
+import java.sql.*;
+
 public class StroreProcedures {
 
+    public static void main(String[] args){
+        try(Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/javaworld","root", ""
+        )){
+
+            //Insert via Result set
+            CallableStatement stmt = conn.prepareCall("CALL country_cities(?)");
+            stmt.setString(1, "Greece");
+
+            ResultSet result = stmt.executeQuery();
+            while(result.next()){
+                System.out.println(result.getString("Name")+ " "
+                        + result.getString("District")+
+                        " " + result.getLong("Population"));
+            }
+            result.close();
+            stmt.close();
+
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+    }
 }
